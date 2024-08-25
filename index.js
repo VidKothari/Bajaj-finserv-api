128b3a2a0d70479ddb8d354ad95be728c2ff9b6b
VidKothari <vidit.kothari@rediffmail.com>
2024-08-25 12:13:05 +0530

    donezzz

--- index.js
+++ index.js
@@ -45,8 +45,9 @@ app.post('/bfhl', (req, res) => {
     });
 });
 
-// app.listen(3000, () => {
-//     console.log('Server is running on port 3000');
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
